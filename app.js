fec9ecc1851b46fa3a9b1039db385dd9dbe1ff28
Joel <joel.rueda.robles.sc@gmail.com>
2020-01-18 18:56:54 -0600

    Paginas dinamicas de Materias

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ db.collection("categoria").onSnapshot((querySnapshot) => {
 page('/FLearningCHP/', index);
 page('/FLearningCHP/categoria/:categoria', categoria);
 page('/FLearningCHP/materia/:materia', materia);
-page('*', notfound);
+// page('*', notfound);
 page();
 
 function index() {
@@ -28,7 +28,7 @@ function index() {
     db.collection("categoria").onSnapshot((querySnapshot) => {
         document.querySelector('main').innerHTML = '';
         document.querySelector('main').innerHTML += `<div id="contentMain" class="card-columns"></div>`;
-        
+
         querySnapshot.forEach((doc) => {
             document.querySelector('#contentMain').innerHTML += `
                 <div class="card shadow">
@@ -48,13 +48,56 @@ function index() {
 
 
 function categoria(ctx) {
-    document.querySelector('main').innerHTML = (ctx.params.categoria);
+
+    var catRef = db.collection("categoria").doc(ctx.params.categoria).get()
+        .then(function (doc) {
+
+            document.querySelector('main').innerHTML = `
+                <div>
+                    <div class="card">
+                        <div class="objetfitcover">
+                            <img class="card-img-top"
+                                src="${doc.data().url_image}"
+                                alt="">
+                                <div class="card-img-overlay m-5 p-5 text-center text-light" style="background-color: rgba(0, 0, 0, 0.5); border-radius: 5px;">
+                                <div class="card-title">
+                                    <h3 class="text-light">${doc.data().nombre}</h3>
+                                </div>
+                                <div class="card-text">
+                                    <small>${doc.data().descripcion}</small>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div id="lista_materias" class="list-group text-dark">
+                                
+                    </div>
+                </div>
+            `;
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+
+
+    db.collection("materia").where("fkid_categoria", "==", ctx.params.categoria).get()
+        .then(function (querySnapshot) {
+            querySnapshot.forEach(function (doc) {
+                // doc.data() is never undefined for query doc snapshots
+                document.querySelector('#lista_materias').innerHTML += `
+                <a href="/FLearningCHP/materia/${doc.id}" class="list-group-item list-group-item-action text-primary">${doc.data().nombre}</a>
+                `;
+            });
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
 }
 
 function materia(ctx) {
-    document.querySelector('#contentMain').innerHTML = 'Hoal';
+    document.querySelector('main').innerHTML = 'Hoal';
 }
 
 function notfound() {
-    document.querySelector('#contentMain').innerHTML = 'Page not found';
+    document.querySelector('main').innerHTML = 'Page not found';
 }
